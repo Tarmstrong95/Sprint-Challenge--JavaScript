2922e19038acc0c5d9793f97a29f849ad639be65
Triston Armstrong <triston95strong@gmail.com>
2019-04-05 12:38:01 -0500

    Complete prototypes

--- challenges/functions.js
+++ challenges/functions.js
@@ -23,9 +23,9 @@ const greeting = (a, b) => `Hello ${a} ${b}, nive to meet you!`
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 
 // steps 1 & 2 required a return, therefore i will log each invocation.
-console.log(consume(2,2,add)); // 4
-console.log(consume(10,16,multiply)); // 160
-console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+// console.log(consume(2,2,add)); // 4
+// console.log(consume(10,16,multiply)); // 160
+// console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
